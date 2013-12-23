c253fb59ef27f0ec274b7c7d3e298e30afa4e968
Martin Micunda <martinmicunda@hotmail.com>
2013-12-23 18:30:51 +0000

    Fix failing build Add Grunt icon Add 3rd dependencies

--- gruntfile.js
+++ gruntfile.js
@@ -2,40 +2,38 @@
 
 module.exports = function(grunt) {
 
-    grunt.loadNpmTasks('grunt-karma');
-    grunt.loadNpmTasks('grunt-contrib-jshint');
-    grunt.loadNpmTasks('grunt-contrib-watch');
-    grunt.loadNpmTasks('grunt-bower-install');
-    grunt.loadNpmTasks('grunt-shell');
-    grunt.loadNpmTasks('grunt-protractor-runner');
-    grunt.loadNpmTasks('grunt-connect');
+    // These plugins provide necessary tasks.
+//    grunt.loadNpmTasks('jshint-stylish');
+
+    // load all grunt tasks matching the `grunt-*` pattern
+    require('load-grunt-tasks')(grunt);
 
     // Project configuration
     grunt.initConfig({
-        distdir: 'dist',
+        distdir: 'build',
         /*
-         * Information can be loaded from the package.json and reused in the
-         * Gruntfile. This is nice in order to avoid duplication of information.
+         * The project metadata that can be loaded from the package.json and reused in the Gruntfile.
          */
         pkg: grunt.file.readJSON('package.json'),
-
-        'bower-install': {
-            target: {
-                // Point to the html file that should be updated
-                // when you run `grunt bower-install`
-                html: 'server/src/app/views/includes/head.jade'
-            }
-        },
-        src: {
+        banner: '/*\n' +
+                ' * TweetMap v<%= pkg.version %> (<%= pkg.homepage %>)\n' +
+                ' * Copyright <%= grunt.template.today("yyyy") %>(c) <%= pkg.author %>\n' +
+                ' * Licensed under <%= _.pluck(pkg.licenses, "type").join(", ") %>\n' +
+                ' */\n\n',
+        /*
+         * The definition of all the sources files and their location.
+         */
+        dirs: {
             gruntfile: 'gruntfile.js',
             server: {
-                jade: ['./server/src/app/view/**/*.jade'],
+                jade: './server/src/app/view/**/*.jade',
                 src: 'server/**/*.js',
                 test: 'test/server/**/*.js'
             },
             client: {
-                js: ['./client/src/app/**/*.js'],
-                css: ['./client/src/assets/*.css'],
+                js: './client/src/app/**/*.js',
+                css: './client/src/assets/**/*.css',
+                img: './client/src/assets/img',
                 tests: {
                     integration: {
                         config: 'test/karma/integration.conf.js',
@@ -46,6 +44,109 @@ module.exports = function(grunt) {
                         src: './client/test/unit/**/*.js'
                     }
                 }
+            }
+        },
+        /*
+         * Clean build directory.
+         */
+        clean: ['<%= distdir %>/*'],
+        /*
+         * Static source code analyses. JSHint verifies that we are adhering
+         * to good coding styles, avoid pitfalls and much more.
+         * http://jshint.com/docs/
+         *
+         * We are configuring different settings for the different parts of our
+         * code, as browser environments are different to NodeJS environments.
+         */
+        jshint: {
+            files: ['<%= dirs.client.js %>'],
+            options: {
+                // use a nice JSHint reporter
+//                reporter: require('jshint-stylish'),
+                // options here to override JSHint defaults
+                "node": true,
+                "browser": true,
+                "esnext": true,
+                "bitwise": true,
+                "curly": true,
+                "eqeqeq": true,
+                "immed": true,
+                "indent": 2,
+                "latedef": true,
+                "newcap": true,
+                "noarg": true,
+                "regexp": true,
+                "undef": true,
+                "unused": true,
+                "trailing": true,
+                "smarttabs": true,
+                "globals": {
+                    "angular": false
+                }
+            }
+        },
+        /*
+         * Static source code analyses. JSHLint verifies that we are adhering
+         * to good coding styles, avoid pitfalls and much more.
+         * http://csslint.net/
+         */
+        csslint: {
+            strict: {
+                options: {
+                    import: 2
+                },
+                src: '<%= dirs.client.css %>'
+            }
+        },
+        /*
+         * Concatenate client javascript files.
+         */
+        concat: {
+            options: {
+                banner: '<%= banner %>'
+            },
+            js: {
+                src: '<%= dirs.client.js %>',
+                dest: "<%= distdir %>/js/<%= pkg.name %>-v<%= pkg.version %>.js"
+            },
+            css: {
+                src: '<%= dirs.client.css %>',
+                dest: "<%= distdir %>/css/<%= pkg.name %>-v<%= pkg.version %>.css"
+            }
+        },
+        /*
+         * Minify JavaScript and CSS to make file sizes as small as possible for production website.
+         */
+        uglify: {
+            options: {
+                banner: "<%= banner %>"
+            },
+            js: {
+                src: '<%= distdir %>/<%= pkg.name %>-v<%= pkg.version %>.js',
+                dest: "<%= distdir %>/<%= pkg.name %>-v<%= pkg.version %>.min.js"
+            }
+//            css: {
+//                src: '<%= distdir %>/<%= pkg.name %>-v<%= pkg.version %>.css',
+//                dest: "<%= distdir %>/<%= pkg.name %>-v<%= pkg.version %>.min.css"
+//            }
+        },
+        /*
+         * Minify PNG, JPEG and GIF images to make file sizes as small as possible for production website.
+         */
+        imagemin: {
+            dynamic: {
+                files: [{
+                    cwd: '<%= dirs.client.img %>',
+                    src: ['**/*.{png,jpg,gif}'],
+                    dest: '<%= distdir %>/img/'
+                }]
+            }
+        },
+        'bower-install': {
+            target: {
+                // Point to the html file that should be updated
+                // when you run `grunt bower-install`
+                html: 'server/src/app/views/includes/head.jade'
             }
         },
         connect: {
@@ -74,39 +175,6 @@ module.exports = function(grunt) {
             },
             npm_install: {
                 command: 'npm install'
-            }
-        },
-        /*
-         * Static source code analyses. JSHint verifies that we are adhering
-         * to good coding styles, avoid pitfalls and much more.
-         * http://jshint.com/docs/
-         *
-         * We are configuring different settings for the different parts of our
-         * code, as browser environments are different to NodeJS environments.
-         */
-        jshint: {
-            files: ['<%= src.gruntfile %>', '<%= src.client.js %>', '<%= src.client.tests %>'],
-            options: {
-                // options here to override JSHint defaults
-                "node": true,
-                "browser": true,
-                "esnext": true,
-                "bitwise": true,
-                "curly": true,
-                "eqeqeq": true,
-                "immed": true,
-                "indent": 2,
-                "latedef": true,
-                "newcap": true,
-                "noarg": true,
-                "regexp": true,
-                "undef": true,
-                "unused": true,
-                "trailing": true,
-                "smarttabs": true,
-                "globals": {
-                    "angular": false
-                }
             }
         },
         watch: {
@@ -203,4 +271,23 @@ module.exports = function(grunt) {
     //installation-related
     grunt.registerTask('install', ['shell:protractor_install']);
     grunt.registerTask('update', ['shell:npm_install', 'concat']);
+
+    grunt.registerTask('build', ['clean','imagemin','concat','uglify']);
+
+    // Generate the production version
+    // ------------------
+    grunt.registerTask('build:production', "Build a minified & production-ready version of your app.", [
+        'clean:dist',
+        'build:dist',
+        'copy:assemble',
+        'createDistVersion'
+    ]);
+    /*
+    * Follow are production steps
+    * 1. Clean & copy files to build folder.. Do I need to that what if I just concate and minifide files straight away to disct folder?
+    * 2. jshint and csshint to verify that js and css syntax is ok
+    * 3. concat js files to one file
+    * 4. Minify files
+    *
+    * */
 };
